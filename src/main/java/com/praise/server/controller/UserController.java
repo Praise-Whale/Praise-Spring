0b1d1b7cfcc7d5a7f6f938e19981993847254486
wjdrbs96 <wjdrbs966@naver.com>
2021-03-23 00:24:19 +0900

    Setting: gitignore add

---
+++ src/main/java/com/praise/server/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.praise.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by jg 2021/03/23
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String test() {
+        return "test";
+    }
+}
